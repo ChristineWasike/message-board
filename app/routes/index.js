7a81f4be7a935ada136afcf84263a49f6c652655
Christine Wasike <christinewasike22@gmail.com>
2017-07-17 18:21:19 +0300

    save methods created.

---
+++ app/routes/index.js
@@ -0,0 +1,22 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model() {
+    return Ember.RSVP.hash({
+      messages: this.store.findAll('message'),
+      answers: this.store.findAll('answer')
+    });
+  },
+  actions: {
+    saveMessage3(params) {
+      var newMessage = this.store.createRecord('message', params);
+      newMessage.save();
+      this.transitionTo('index');
+    },
+    saveAnswer(params) {
+      var newAnswer = this.store.createRecord('answer', params);
+      newAnswer.save();
+      this.transitionTo('index');
+    }
+  }
+});
