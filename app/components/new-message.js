1f8e8a192e21504fbfc04ed766ece126e5b95ca2
Christine Wasike <christinewasike22@gmail.com>
2017-07-24 18:33:08 +0300

    new message hbs file added, it contains the modal that has the form to file when creating a new message for the riddle

--- app/components/new-message.js
+++ app/components/new-message.js
@@ -1,20 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  addNewMessage: false,
   actions: {
-    messageFormShow() {
-      this.set('addNewMessage', true);
-    },
-    saveMessage1() {
+    saveMessage() {
       var params = {
         riddle: this.get('riddle'),
         author: this.get('author'),
         hint: this.get('hint'),
         image:  this.get('image'),
       };
-      this.set('addNewMessage', false);
-      this.set('saveMessage2', params);
+      this.sendAction('saveMessage', params);
     }
   }
 });
