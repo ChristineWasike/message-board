9655852523ef02c1d94e613a99b4cab415361c99
Christine Wasike <christinewasike22@gmail.com>
2017-07-24 07:56:29 +0300

    Delete function created to get rid of the answer

--- app/components/answer-tile.js
+++ app/components/answer-tile.js
@@ -3,9 +3,11 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   actions: {
     delete(answer) {
-      if (confirm('Are you sure you want to delete your answer?')) {
+      if (confirm('Are you sure you want to delete this answer?')) {
         this.sendAction('destroyAnswer', answer);
       }
-    }
+    },
+    
+
   }
 });
